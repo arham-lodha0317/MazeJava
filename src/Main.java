59df04510902202de97c97e585915cf5f52dfc04
arham-lodha <arhamsmita@gmail.com>
2020-05-06 17:13:57 -0500

    Tiny bug fix

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args){
-
-        Maze maze = new Maze(new Picture(new File("mazes/braid200.png")));
-        maze.check().save("check.png");
+        //enter Maze Names
+        String name = "small";
+        Maze maze = new Maze(new Picture(new File("mazes/"+name+".png")));
+        maze.check().save("Nodes/"+name+".png");
         System.out.printf("Load Factor: %f\n", maze.loadFactor());
-        maze.generatePicture(maze.AStar()).save("check2.png");
+        maze.generatePicture(maze.AStar()).save("Solved/"+name+".png");
 
 
 
